373c15cc557e68e7437b382e2650c1df7083a87a
Rahul Wakalkar <rahulwakalkar99@gmail.com>
2025-05-26 11:57:51 +0530

    data transformation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
 
 if __name__=="__main__":
@@ -12,6 +13,10 @@ if __name__=="__main__":
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        
+        data_transformation=DataTransformation()
+        #train_arr,test_arr=
+        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 #from sklearn.model_selection import GridSearchCV
 #from sklearn.metrics import r2_score
 import pymysql
+import pickle
+
 
 #import pickle
 import numpy as np
@@ -34,8 +36,27 @@ def read_sql_data():
         print(df.head())
 
         return df
-
-
-
     except Exception as ex:
         raise CustomException(ex)
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+    
+
+
+
+
+    
+    
